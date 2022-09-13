873ebc49460dd6160df3021533f384d60d796504
JohnGolgota <js684new@gmail.com>
2022-09-13 18:23:48 -0500

    I give up

---
+++ Models/tbl_libros.php
@@ -0,0 +1,15 @@
+id_li titulo_li tituloor_li fpublic_li orderse_li npag estado_li com_li fcom_li id_ed 
+<?php
+class tbl_libors{
+    protected $id_li;
+    protected $titulo_li;
+    protected $tituloor_li;
+    protected $fpublic_li;
+    protected $orderse_li;
+    protected $npag_li;
+    protected $estado_li;
+    protected $com_li;
+    protected $fcom_li;
+    protected $id_ed;
+}
+?>

cee8248337500b3e5ffb033746afd14930246f1e
JohnGolgota <js684new@gmail.com>
2022-09-17 11:31:19 -0500

    por fin el libro

--- Models/tbl_autor.php
+++ Models/tbl_autor.php
@@ -12,8 +12,14 @@ class tbl_autor{
     public function RegistrarAutor(){
         include_once '../Config/conectdb.php';
         $conexion = new Conexion();
-        $sql = "INSERT INTO tbl_autor (nombre_au,seudonimo_au,nacio_au,fnacio_au,ocupacion_au,lenguao_au) VALUES ('$this->nombre_au','$this->seudonimo_au','$this->nacio_au','$this->fnacio_au','$this->ocupacion_au','$this->lenguao_au')";
+        $sql = "INSERT INTO tbl_autor (nombre_au,seudonimo_au,nacio_au,fnacio_au,ocupacion_au,lenguao_au) VALUES (?,?,?,?,?,?)";
         $insert = $conexion->stm->prepare($sql);
+        $insert->bindParam(1,$this->nombre_au);
+        $insert->bindParam(2,$this->seudonimo_au);
+        $insert->bindParam(3,$this->nacio_au);
+        $insert->bindParam(4,$this->fnacio_au);
+        $insert->bindParam(5,$this->ocupacion_au);
+        $insert->bindParam(6,$this->lenguao_au);
         $insert->execute();
     }
     public function AllAuthors()
--- Models/tbl_libros.php
+++ Models/tbl_libros.php
@@ -3,12 +3,14 @@ class tbl_libros{
     protected $id_li;
     protected $titulo_li;
     protected $tituloor_li;
-    protected $fpublic_li;
+    protected $serie_li;
     protected $orderse_li;
+    protected $arte_li;
+    protected $artista_li;
+    protected $fpublic_li;
     protected $npag_li;
     protected $estado_li;
     protected $com_li;
     protected $fcom_li;
-    protected $id_ed;
 }
 ?>

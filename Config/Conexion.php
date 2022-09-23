228b21e26074296a8c94400c4204c0799f14b97e
JohnG <js684new@gmail.com>
2022-09-23 17:32:14 -0500

    adios entrega de fase

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -1,13 +1,47 @@
 <?php
 
-class Conexion{
+class Conexion
+{
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+
     public $stm;
-    public function __construct(){
-        try{
+    public function __construct()
+    {
+        try {
             $this->stm = new PDO("mysql:host=localhost;dbname=colection", 'root', '');
-        } catch(PDOException $error) {
-            echo "Error: ". $error->getMessage();
+        } catch (PDOException $error) {
+            echo "Error: " . $error->getMessage();
+            $this->CrearBD();
         }
     }
+    public function CrearBD()
+    {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "";
+
+        // Create connection
+        $connect = new mysqli($servername, $username, $password);
+        // Check connection
+        if ($connect->connect_error) {
+            die("Connection failed: " . $connect->connect_error);
+        }
+
+        // Create database
+        $sql = "CREATE DATABASE colection";
+        if ($connect->query($sql) === TRUE) {
+            echo "Database created successfully";
+            CrearTables();
+        } else {
+            echo "Error creating database: " . $connect->error;
+        }
+
+        $connect->close();
+    }
+    public function CrearTables()
+    {
+        
+    }
 }
-?>

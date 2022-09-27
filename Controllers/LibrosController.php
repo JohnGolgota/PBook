369898a9ba4be145fc1e57e3596ed13a8f2304cd
JohnGolgota <js684new@gmail.com>
2022-09-27 09:02:26 -0500

    perdi el Mov

--- Controllers/LibrosController.php
+++ Controllers/LibrosController.php
@@ -6,6 +6,10 @@ class LibrosController extends tbl_libros{
     {
         include_once '../Views/Libros/Libros.php';
     }
+    public function RedirigirColeccion()
+    {
+        header("location: LibrosController.php?c=".$_SESSION['correo_usr']."&u=".$_SESSION['nombre_usr']);
+    }
     public function VistaAgregarLibro()
     {
         // Esto no esta bien
@@ -15,7 +19,87 @@ class LibrosController extends tbl_libros{
         include_once  '../Models/tbl_genero.php';
         $genero = new tbl_genero();
         $objetoGeneros = $genero->AllGeneros();
+        $objetoLibros = $this->AllLibros();
         include_once '../Views/Libros/Agregar.php';
+    }
+    public function PrepararInformacion()
+    {
+        // if ($_POST['id_li'] != "") {
+            // echo "que?";
+            // }
+        // if ($_POST['id_li'] == "") {
+        //     $_POST['id_li'] = "";
+        // }
+        $this->id_li = $_POST['id_li'];
+
+        if ($_POST['titulo_li'] == "") {
+            $_POST['titulo_li'] = "El titulo";
+        }
+        $this->titulo_li = $_POST['titulo_li'];
+
+        if ($_POST['tituloor_li'] == "") {
+            $_POST['tituloor_li'] = "Sin titulo Original";
+        }
+        $this->tituloor_li = $_POST['tituloor_li'];
+
+        if ($_POST['npag_li'] == "") {
+            $_POST['npag_li'] = "0";
+        }
+        $this->npag_li = $_POST['npag_li'];
+
+        if ($_POST['serie_li'] == "") {
+            $_POST['serie_li'] = "Independiente";
+        }
+        $this->serie_li = $_POST['serie_li'];
+
+        if ($_POST['orderse_li'] == "") {
+            $_POST['orderse_li'] = "0";
+        }
+        $this->orderse_li = $_POST['orderse_li'];
+
+        if ($_POST['arte_li'] == "") {
+            $_POST['arte_li'] = "Arte";
+        }
+        $this->arte_li = $_POST['arte_li'];
+
+        if ($_POST['artista_li'] == "") {
+            $_POST['artista_li'] = "Desconocido";
+        }
+        $this->artista_li = $_POST['artista_li'];
+
+        if ($_POST['fpublic_li'] == "") {
+            $_POST['fpublic_li'] = date("Y-m-d");
+        }
+        $this->fpublic_li = $_POST['fpublic_li'];
+
+        if ($_POST['estado_li'] == "") {
+            $_POST['estado_li'] = "secondary";
+        }
+        $this->estado_li = $_POST['estado_li'];
+
+        if ($_POST['com_li'] == "") {
+            $_POST['com_li'] = "No";
+        }
+        $this->com_li = $_POST['com_li'];
+
+        if ($_POST['fcom_li'] == "") {
+            $_POST['fcom_li'] = "0000-00-00";
+        }
+        $this->fcom_li = $_POST['fcom_li'];
+
+        if ($_POST['id_au'] == "") {
+            $_POST['id_au'] = "";
+        }
+        $this->id_au = $_POST['id_au'];
+
+        if ($_POST['id_ge'] == "") {
+            $_POST['id_ge'] = "";
+        }
+        $this->id_ge =  $_POST['id_ge'];
+    }
+    public function PrepararInsert()
+    {
+        $this->AgregarLibro();
     }
 }
 if (isset($_GET['c'], $_GET['u'])) {
@@ -27,11 +111,17 @@ if (isset($_GET['action']) && $_GET['action'] == 'agregar') {
     $libroscontroller->VistaAgregarLibro();
     return;
 }
-if (isset($_POST['action']) && $_POST['action'] == 'prueba') {
-    echo $_POST['listaautores_li'];
-    // $libroscontroller = new LibrosController();
-    // $libroscontroller->VistaAgregarLibro();
+if (isset($_POST['action']) && $_POST['action'] == 'insert' && !empty($_POST['titulo_li'])) {
+    echo "insertar libro";
+    var_dump($_POST);
+    $libroscontroller = new LibrosController();
+    $libroscontroller->PrepararInformacion();
+    $libroscontroller->PrepararInsert();
+    $libroscontroller->RedirigirColeccion();
     return;
+}
+if (isset($_POST['action']) && $_POST['action'] == 'insert' && empty($_POST['titulo_li'])) {
+    echo "Pon el titulo... solo eso";
 }
 if (!isset($_GET) || isset($_GET)) {
     $libroscontroller = new LibrosController();
--- Models/tbl_libros.php
+++ Models/tbl_libros.php
@@ -13,5 +13,31 @@ class tbl_libros{
     protected $com_li;
     protected $fcom_li;
     protected $id_au;
+    
+    protected $id_ge;
+    protected $tbl_genero_libro;
+
+    public function AllLibros()
+    {
+        include_once '../Config/Conexion.php';
+        $conexion = new Conexion();
+        $sql = "SELECT * FROM tbl_libros";
+        $prepare = $conexion->stm->prepare($sql);
+        $prepare->execute();
+
+        $objetoLibros = $prepare->fetchAll(PDO::FETCH_OBJ);
+        return $objetoLibros;
+    }
+    public function AgregarLibro()
+    {
+        include_once '../Config/Conexion.php';
+        $conexion = new Conexion();
+        $sql = "INSERT INTO tbl_libros(titulo_li,tituloor_li,serie_li,orderse_li,arte_li,artista_li,fpublic_li,npag_li,estado_li,com_li,fcom_li,id_au) VALUES ('$this->titulo_li','$this->tituloor_li','$this->serie_li','$this->orderse_li','$this->arte_li','$this->artista_li','$this->fpublic_li','$this->npag_li','$this->estado_li','$this->com_li','$this->fcom_li','$this->id_au'); INSERT INTO tbl_genero_libro(id_ge,id_li) VALUES ('$this->id_ge','$this->id_li')";
+        $prepare = $conexion->stm->prepare($sql);
+        $prepare->execute();
+
+        $objetoLibros = $prepare->fetchAll(PDO::FETCH_OBJ);
+        return $objetoLibros;
+    }
 }
 ?>
